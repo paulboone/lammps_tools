33f27e117047007e16d7ea0a89bee49bd6e189a3
Paul Boone <narcissus.pong@gmail.com>
2017-07-25 13:43:57 -0400

    print new plot every N rows

--- lammps_to_plot.py
+++ lammps_to_plot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from math import ceil
 import sys
 
 from matplotlib import pyplot as plt
@@ -10,6 +11,7 @@ parser.add_argument('filepath', help="Path to LAMMPS chunks output file")
 parser.add_argument("--ylabel", "--yl", default="Unspecified Varname", help="variable measured in the LAMMPS file")
 parser.add_argument("--xlabel", "--xl", default="chunk", help="chunk dimension")
 parser.add_argument("--rows", "-r", nargs=2, default=None, help="Start and stop rows. Defaults to plotting all rows.")
+parser.add_argument("--plot-every", "-p", default=None, help="Number of rows per plot.")
 parser.add_argument("--yrange", "--yr", nargs=2, default=None, help="Y Range. Defaults to total range of entire dataset.")
 parser.add_argument("--xrange", "--xr", nargs=2, default=None, help="X Range. Defaults to num of chunks in LAMMPS file.")
 args = parser.parse_args()
@@ -42,14 +44,9 @@ if values:
 if args.rows:
     row_start = int(args.rows[0])
     row_stop = int(args.rows[1])
-    # ts_size = int(args.timestep_size)
-
-    rows = rows[row_start:row_stop]
-    values_by_rows = values_by_rows[row_start:row_stop]
 else:
     row_start = 0
     row_stop = len(rows)
-    # ts_size = 1
 
 x_range = (1, int(num_chunks))
 if args.xrange:
@@ -67,23 +64,36 @@ else:
 alpha_min = 0.10
 alpha_max = 0.50
 
+if args.plot_every:
+    plot_every = int(args.plot_every)
+else:
+    plot_every = row_stop - row_start
 
-fig = plt.figure(figsize=(8.0,6.0))
-ax = fig.add_subplot(111)
+num_plots = ceil((row_stop - row_start) / plot_every)
 
-ax.set_title("%s by %s [rows %s-%s]" % (args.ylabel, args.xlabel, row_start, row_stop))
-ax.set_ylim(y_range)
-ax.set_xlim(x_range)
+fig = plt.figure(figsize=(8.0,4.0 * num_plots))
+x_range = [1, len(values_by_rows[1])]
 
-x_range = range(1, len(values) + 1)
-for i, ts in enumerate(rows):
-    if i == len(rows) - 1:
-        alpha = 1.00
-        width = 4
-    else:
-        alpha = alpha_min + i*(alpha_max - alpha_min)/len(rows)
-        width = 0.5
+for plot_index in range(1, num_plots + 1):
+    print(plot_index)
+    ax = fig.add_subplot(num_plots, 1, plot_index)
+    ax.set_ylim(y_range)
+    ax.set_xlim(x_range)
 
-    ax.plot(x_range, values_by_rows[i], 'b', alpha=alpha, lw=width)
+
+    grow_start = row_start + plot_every * (plot_index - 1)
+    grow_stop = min(row_start + plot_every * (plot_index), row_stop)
+
+    ax.set_title("%s by %s [rows %s-%s]" % (args.ylabel, args.xlabel, grow_start, grow_stop))
+
+    for i in range(grow_start, grow_stop):
+        if i == grow_stop - 1:
+            alpha = 1.00
+            width = 4
+        else:
+            alpha = alpha_min + (i - grow_start)*(alpha_max - alpha_min)/(grow_stop - grow_start)
+            width = 0.5
+
+        ax.plot(range(x_range[0], x_range[-1] + 1), values_by_rows[i], 'b', alpha=alpha, lw=width)
 
 fig.savefig(args.filepath + ".png", dpi=144)
