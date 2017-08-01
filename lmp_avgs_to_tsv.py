2a4861fd11bf53abcd6ceef8ef124c66c1a6ffbf
Paul Boone <narcissus.pong@gmail.com>
2017-08-01 14:25:12 -0400

    avgs to tsv

---
+++ lmp_avgs_to_tsv.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import argparse
+import csv
+import sys
+
+from utils import thermo_from_lammps_log
+
+parser = argparse.ArgumentParser("./lmp_log_to_tsv.py")
+parser.add_argument('filepaths', nargs='+', help="Path to LAMMPS averages file(s)")
+args = parser.parse_args()
+
+filenames = args.filepaths
+
+cols = []
+last_timestep = -1
+tsv = csv.writer(sys.stdout, delimiter="\t")
+
+for filename in filenames:
+    with open(filename, 'r') as f:
+        _ = next(f)
+        cols1 = next(f)[2:].strip().split()
+        if not cols:
+            cols = cols1
+            tsv.writerow(cols)
+        elif cols != cols1:
+            raise Exception("columns of filename %s do not match prior files: %s != %s" % (filename, cols, cols1))
+
+        for line in f:
+            tsv.writerow(line.strip().split())
