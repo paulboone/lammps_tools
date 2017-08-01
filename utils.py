3a756f124e71d9c763fbf369adc5ae8ed093fc46
Paul Boone <narcissus.pong@gmail.com>
2017-08-01 13:11:38 -0400

    update lammps_to_tsv with argparse, multiple files

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@
 
 
 
-def thermo_from_lammps_log(f, last_timestep=-1):
+def thermo_from_lammps_log(f, last_timestep=-1, verbose=False):
     found_data = False
     cols = []
     data = []
@@ -13,24 +13,24 @@ def thermo_from_lammps_log(f, last_timestep=-1):
                 pass
             else:
                 cols = line.strip().split()
-                print("found columns: %s" % cols)
+                if verbose:
+                    print("found columns: %s" % cols)
                 found_data = True
         else:
             if not line.startswith("Loop"):
                 raw_data = line.strip().split()
-                raw_data[0] = int(raw_data[0])
-                for i in range(1,len(raw_data)):
-                    raw_data[i] = float(raw_data[i])
 
                 if raw_data[0] == last_timestep:
-                    print("INFO: skipping first line of new file b/c timestep is the same.")
+                    if verbose:
+                        print("INFO: skipping first line of new file b/c timestep is the same.")
                 else:
                     data.append(raw_data)
             else:
                 break
 
     if len(data) > 1 and len(data[-1]) < len(data[-2]):
-        print("INFO: removing last row of data because it is imcomplete")
+        if verbose:
+            print("INFO: removing last row of data because it is imcomplete")
         data.pop()
 
     return cols, data
