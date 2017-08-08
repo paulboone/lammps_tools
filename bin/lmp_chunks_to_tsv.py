1b1b537758cdf16da384fd13a0e5fbc20915281f
Paul Boone <narcissus.pong@gmail.com>
2017-08-08 02:14:41 -0400

    lmp_chunks_to_tsv now can handle multiple files

--- bin/lmp_chunks_to_tsv.py
+++ bin/lmp_chunks_to_tsv.py
@@ -5,7 +5,7 @@ import csv
 import sys
 
 parser = argparse.ArgumentParser("./lmp_chunks_to_tsv.py") #help='Process LAMMPS chunks file and plot'
-parser.add_argument('filepath', help="Path to LAMMPS chunks output file")
+parser.add_argument('filenames', nargs='+', help="Path(s) to LAMMPS chunk output file(s)")
 
 args = parser.parse_args()
 
@@ -13,26 +13,32 @@ rows = []
 values_by_rows = []
 values = []
 
-with open(args.filepath, 'r') as f:
-    for line in f:
-        if line.startswith('#'):
-            pass
-        elif line[0].isdigit():
-            if values:
-                rows.append(timestep)
-                values_by_rows.append(values)
-                values = []
-            timestep, num_chunks, _ = line.split()
-            num_chunks = int(num_chunks)
-        elif line.strip() == "":
-            pass
-        else:
-            _,_,_, temp = line.split()
-            values.append(temp)
+for filename in args.filenames:
+    with open(filename, 'r') as f:
+        for line in f:
+            if line.startswith('#'):
+                pass
+            elif line[0].isdigit():
+                if values:
+                    rows.append(timestep)
+                    values_by_rows.append(values)
+                    values = []
+                timestep, num_chunks, _ = line.split()
+                num_chunks = int(num_chunks)
+            elif line.strip() == "":
+                pass
+            else:
+                _,_,_, chunkval = line.split()
+                values.append(chunkval)
 
-if values:
-    rows.append(timestep)
-    values_by_rows.append(values)
+    if values:
+        print(values_by_rows[-1])
+        if len(values) != len(values_by_rows[-1]):
+            raise Exception("# of chunks does not match in file %s timestep %s (%s != %s)" % (filename, timestep, len(values), len(values_by_rows[-1])))
+
+        rows.append(timestep)
+        values_by_rows.append(values)
+
 
 tsv = csv.writer(sys.stdout, delimiter="\t")
 tsv.writerow(['Timestep'] + list(range(1, num_chunks + 1)))
