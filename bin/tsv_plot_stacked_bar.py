18066a690f3b69aa7f9609bd57be9033150d9ef1
Paul Boone <narcissus.pong@gmail.com>
2017-08-21 11:24:09 -0400

    add title

--- bin/tsv_plot_stacked_bar.py
+++ bin/tsv_plot_stacked_bar.py
@@ -17,6 +17,7 @@ parser.add_argument("--avg-every", "-a", default=1, help="Number of rows to aver
 parser.add_argument("--yrange", "--yr", nargs=2, default=None, help="Y Range. Defaults to total range of entire dataset.")
 parser.add_argument("--xrange", "--xr", nargs=2, default=None, help="X Range. Defaults to num of chunks in LAMMPS file.")
 parser.add_argument("--columns", "-c", nargs=2, action='append', metavar=('idx', 'label'), help="<column #> <label>")
+parser.add_argument("--title", default=None, help="Defaults to '<ylabel> by <xlabel>'")
 parser.add_argument("--ylabel", "--yl", default="Heat Flux [Kcal / mol A^2]", help="total of all columns")
 parser.add_argument("--xlabel", "--xl", default="Timestep", help="x dimension")
 parser.add_argument("--timesteps-per-row", "-t", default=10000, help="timesteps per row. Defaults to 10000.")
@@ -85,17 +86,26 @@ col_names = [c[1] for c in columns]
 
 for plot_index in range(0, 1):
     ax = fig.add_subplot(num_plots, 1, plot_index + 1)
-    ax.set_title("%s by %s" % (args.ylabel, args.xlabel))
+
+    if args.title:
+        ax.set_title(args.title)
+    else:
+        ax.set_title("%s by %s" % (args.ylabel, args.xlabel))
+    ax.set_xlabel(args.xlabel)
+    ax.set_ylabel(args.ylabel)
+
     ax.grid(linestyle='-', color='0.7', zorder=0)
+    ax.set_xticks(bar_x + bar_width / 2)
+    ax.set_xticklabels(x_labels)
     ax.set_ylim(y_range)
     ax.set_xlim(x_range)
+
     prior_vals = np.zeros(len(rows))
     for col_index, col_name in columns:
         vals = values_by_rows[:,col_index]
         ax.bar(bar_x, vals, bar_width, color=colors[col_index], bottom=prior_vals, zorder=3)
         prior_vals += vals
-    ax.set_xticks(bar_x + bar_width / 2)
-    ax.set_xticklabels(x_labels)
+
     ax.legend(col_names)
 
 
